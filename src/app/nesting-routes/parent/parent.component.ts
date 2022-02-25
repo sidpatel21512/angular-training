5f957b89687d6eca6388d1177d85c85c5955591c
Siddharth Patel <siddharth.patel@CDSYS.LOCAL>
2022-02-25 18:17:06 +0530

    Add relative route example

--- src/app/nesting-routes/parent/parent.component.ts
+++ src/app/nesting-routes/parent/parent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-parent',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
-  constructor() { }
+  private isFirst = false;
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+  // Relative route example
+  goToOtherRoute() {
+    const path = this.isFirst ? 'child-a' : 'child-b';
+    this.isFirst = !this.isFirst;
+    this.router.navigate([path], {relativeTo: this.route});
   }
 
 }
