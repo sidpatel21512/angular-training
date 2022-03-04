d616c638ea191bc3c7bfcd03069cde4a92d9d69a
Siddharth Patel <siddharth.patel@CDSYS.LOCAL>
2022-03-04 19:54:22 +0530

    Fix issue with ngAfterContentInit and ngAfterContentChecked example

--- src/app/life-cycle-hooks/after-content-init/init-child/init-child.component.ts
+++ src/app/life-cycle-hooks/after-content-init/init-child/init-child.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentChecked, AfterContentInit, Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, Component, ContentChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-init-child',
@@ -6,6 +6,9 @@ import { AfterContentChecked, AfterContentInit, Component, OnInit } from '@angul
   styleUrls: ['./init-child.component.scss']
 })
 export class InitChildComponent implements OnInit, AfterContentInit, AfterContentChecked {
+
+  @ContentChild("firstcontent", { read: ElementRef }) fRef!: ElementRef;
+  @ContentChild("seccontent", { read: ElementRef }) sRef!: ElementRef;
 
   constructor() {
     console.log('constructor:');
@@ -17,9 +20,13 @@ export class InitChildComponent implements OnInit, AfterContentInit, AfterConten
 
   ngAfterContentInit(): void {
     console.log('ngAfterContentInit:');
+    console.log('fRef:', this.fRef);
+    console.log('sRef:', this.sRef);
   }
 
   ngAfterContentChecked(): void {
     console.log('ngAfterContentChecked:');
+    console.log('fRef:', this.fRef);
+    console.log('sRef:', this.sRef);
   }
 }
